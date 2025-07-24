216a5d78adf1e62cc90a226ae297803dac98a4c0
Saurav <ksaurav4093@gmail.com>
2025-07-24 06:52:11 +0530

    fix: adjust background image position in Home component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="bg-zinc-950 relative w-full min-h-screen overflow-hidden">
       
       {/* Background image with non-blocking overlay */}
-      <div className="absolute -top-[130vh] inset-0 z-0  pointer-events-none select-none">
+      <div className="absolute -top-[10vh] inset-0 z-0  pointer-events-none select-none">
         <Image
           src="/Gradient.svg"
           alt="Background"
